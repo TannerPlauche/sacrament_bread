aceca182519a151e09cada1a088b8ba49b730cb7
Tanner Plauche <tanner@diversyfund.com>
2022-12-05 15:50:01 -0600

    init

---
+++ index.js
@@ -0,0 +1,108 @@
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import Twilio from 'twilio';
+dotenv.config();
+
+// data here
+// https://www.apispreadsheets.com/files/aYWNP5qJD6BhWtFE
+
+// twilio data
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const token = process.env.TWILIO_TOKEN;
+
+const tanner = {
+    firstName: 'Tanner',
+    lastName: 'Plauché',
+    phone: '7202221037'
+}
+
+const tyler = {
+    firstName: 'Tanner',
+    lastName: 'Plauché',
+    phone: '7202221037'
+}
+
+const twilioAccount = {
+    firstName: 'Tanner',
+    lastName: 'Plauché',
+    phone: '13608032968'
+}
+
+const twilio = new Twilio(accountSid, token);
+
+/**
+ * send message via twilio
+ * @param {string} recipientPhone 
+ * @param {string} senderPhone 
+ * @param {string} message 
+ */
+async function sendMessage(recipientPhone, senderPhone, message) {
+
+    console.log(`sending message to ${recipientPhone} from ${senderPhone}`);
+    const promise = twilio.messages.create({
+        from: recipientPhone,
+        to: senderPhone,
+        body: message
+    });
+
+    promise.then(function (message) {
+        console.log('Created message using promises');
+        console.log(message.sid);
+    }).catch(err => console.log(err));
+}
+
+TWILIO_TOKEN
+async function getData() {
+    return fetch(`https://api.apispreadsheets.com/data/${process.env.FILE_ID}/`).then(res => {
+        if (res.status === 200) {
+            console.log('success');
+            return res.json().then(data => {
+                const yourData = data.data;
+                return yourData;
+            }).catch(err => console.log(err))
+        }
+        else {
+            console.log(`err: ${err}`);
+        }
+    });
+}
+
+export const handler = async () => {
+
+    const data = await getData();
+    const today = new Date();
+    const filteredData = data.filter(sched => new Date(sched.date) > today);
+    // console.log('filteredData: ', filteredData);
+
+    const nextSunday = filteredData[0];
+    console.log('nextSunday: ', nextSunday);
+    if (!nextSunday) {
+        process.exit();
+    }
+
+    const message = `Hi ${nextSunday.firstName}, you are scheduled to bring the bread for sacrament this Sunday, ${nextSunday.date}. If you are not able to do this please
+let Brother Plauché (${tanner.phone}) or Brother Nelson (${tyler.phone}) know. \n\n\nyou will receive this message Friday, Saturday, and Sunday the week leading up to ${nextSunday.date}`
+
+    console.log('message: ', message);
+
+    await sendMessage(
+        nextSunday.phone,
+        twilioAccount.phone,
+        message
+    );
+
+    await sendMessage(
+        tanner.phone,
+        twilioAccount.phone,
+        message
+    );
+
+    await sendMessage(
+        tyler.phone,
+        twilioAccount.phone,
+        message
+    );
+
+}
+
+
